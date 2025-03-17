3b024f98be79efb5013e54aa2afd9dcb76d30315
Parames1227 <parameswar12102004@gmail.com>
2025-03-18 01:27:44 +0530

    Time: 5 ms (48.89%), Space: 44.3 MB (72.09%) - LeetHub

---
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean divideArray(int[] nums) 
+    {
+        Arrays.sort(nums);
+        for (int i = 0; i< nums.length; i = i+2)
+        {
+            if(nums.length %2 == 0)
+            {
+                if(nums[i]!= nums[i+1])
+                {
+                    return false;
+                }
+                
+            }
+        }
+        return true;
+    }
+}
