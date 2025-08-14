54ec3dfa4bd25bbcee47ebcdd679288a1a3ed775
Parames1227 <parameswar12102004@gmail.com>
2025-08-14 23:58:32 +0530

    Time: 0 ms (100%), Space: 43.3 MB (13.42%) - LeetHub

---
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int ans = Integer.MIN_VALUE;
+        for(int[] i : accounts)
+        {
+            int sum=0;
+            for(int j: i)
+            {
+                sum+=j;
+            }
+            if(sum >ans)
+            {
+                ans=sum;
+            }
+        }
+        return ans;
+
+    }
+}
